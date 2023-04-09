7c183bd853dfb684be1bafecaec1f85ae59154f2
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-09 07:10:03 +0000

    Add some assets

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as LeftCard }     from './LeftCard/LeftCard';
+export { default as Introduction } from './Introduction/Introduction';
---
+++ src/constants/images.js
@@ -0,0 +1,9 @@
+import profile from '../assets/profile.jpg';
+import banner  from '../assets/banner.jpg';
+
+const images = {
+    profile,
+    banner,
+};
+
+export default images;
