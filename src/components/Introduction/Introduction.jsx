f1279f1fcfc7db1e525879535e9aafe7577a703c
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-09 10:37:48 +0000

    Implemente the Introduction section

---
+++ src/components/Introduction/Introduction.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Introduction.css';
+
+
+
+const Introduction = () => {
+  return (
+    <div className='app__intro'>
+      <h3>Introduction</h3>
+      <p>
+        I'm a Creative Developer & Technology Passionate
+      </p>
+      <div className='bar' />
+    </div>
+  );
+};
+
+export default Introduction;
