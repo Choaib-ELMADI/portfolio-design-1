f107df709f0f207d8443fb027f554661ad801a9c
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-11 15:16:49 +0000

    Styling and animating the leftCard

--- src/components/LeftCard/LeftCard.jsx
+++ src/components/LeftCard/LeftCard.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import images from '../../constants/images';
+import { useStateContext } from '../../StateContext/StateContext';
 import './LeftCard.css';
+import './LeftCardSmall.css';
 
 
 
 const LeftCard = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const { visible } = useStateContext();
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(width <= 800px)');
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaChange = (e) => {
+      setIsMobile(e.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaChange);
+    };
+  }, []);
+
   return (
-    <div className='app__leftcard'>
-      <div className="image-container">
-        <div className='border border-1' />
-        <div className='border border-2' />
-        <div className='border border-3' />
-        <div className="profile">
-          <img src={ images.profile } draggable={ false } alt="Choaib-ELMADI" />
-          <div className='title'>
-            <h2>Choaib ELMADI</h2>
-            <span />
-          </div>
+    <>
+      {
+        !isMobile ?
+        <div className='app__leftcard'>
+          <div className="image-container">
+            <div className='border border-1' />
+            <div className='border border-2' />
+            <div className='border border-3' />
+            <div className="profile">
+              <img src={ images.profile } draggable={ false } alt="Choaib-ELMADI" />
+              <div className='title'>
+                <h2>Choaib ELMADI</h2>
+                <span />
+              </div>
+            </div>
+            </div>
+        </div> : 
+        <div className={ visible ? 'app__leftcard-small' : 'app__leftcard-small skew' }>
+          <div className="profile">
+              <img src={ images.profile } draggable={ false } alt="Choaib-ELMADI" />
+              <div className='title'>
+                <h1>Choaib ELMADI</h1>
+                <span />
+              </div>
+            </div>
         </div>
-        </div>
-    </div>
+      }
+    </>
   );
 };
 
