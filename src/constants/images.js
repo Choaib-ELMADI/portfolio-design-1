036b39074713e4abb2e161e0bceec442eb2059be
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-11 18:19:51 +0000

    Add and import assets

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,23 @@
 import profile from '../assets/profile.jpg';
 import banner  from '../assets/banner.jpg';
 
+import image1  from '../assets/1.png';
+import image2  from '../assets/2.png';
+import image4  from '../assets/4.png';
+import image5  from '../assets/5.png';
+import image7  from '../assets/7.png';
+import image8  from '../assets/8.png';
+
 const images = {
     profile,
     banner,
+
+    image1,    
+    image2,    
+    image4,    
+    image5,    
+    image7,    
+    image8,    
 };
 
 export default images;
