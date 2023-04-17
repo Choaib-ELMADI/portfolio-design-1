c0b6f66bfef1da3c1706a1c40781a1e1356d829d
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-04-17 15:23:57 +0000

    Change the style one last time

--- src/components/Sections/Introduction/Introduction.jsx
+++ src/components/Sections/Introduction/Introduction.jsx
@@ -11,7 +11,17 @@ const Introduction = () => {
     <div className='app__intro' id='home'>
       <h3>Introduction</h3>
       <p>
-        I'm a<span style={{ color: selectedColor }}> Creative </span>Developer & Technology<span style={{ color: selectedColor }}> Passionate</span>.
+        I'm a <span 
+          style={{ 
+            textDecoration: `underline 4px ${ selectedColor }`,
+            letterSpacing: '-2px'
+          }}
+        >Creative</span> Developer & Technology <span 
+          style={{ 
+            textDecoration: `underline 4px ${ selectedColor }`, 
+            letterSpacing: '-2px'
+          }}
+        >Passionate</span>.
       </p>
       <div className='bar' />
     </div>
